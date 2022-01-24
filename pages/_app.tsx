34648307a45dd85274b09be4d7a17903bd3a7789
iqballbayhaqi <baihaqiiqbal323@gmail.com>
2022-01-25 05:00:55 +0700

    Refactor login Navbar Component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,20 @@
-import "../styles/homepage.css";
-import "../styles/utilities.css";
-import "../styles/detail.css";
-import "../styles/checkout.css";
-import "../styles/complete-checkout.css";
-import "../styles/sign-in.css";
-import "../styles/sign-up.css";
-import "../styles/sign-up-photo.css";
-import "../styles/404-not-found.css";
-import "../styles/overview.css";
-import "../styles/sidebar.css";
-import "../styles/transactions.css";
-import "../styles/transactions-detail.css";
-import "../styles/edit-profile.css";
-import type { AppProps } from "next/app";
-import Head from "next/head";
+import '../styles/homepage.css';
+import '../styles/utilities.css';
+import '../styles/detail.css';
+import '../styles/checkout.css';
+import '../styles/complete-checkout.css';
+import '../styles/sign-in.css';
+import '../styles/sign-up.css';
+import '../styles/sign-up-photo.css';
+import '../styles/404-not-found.css';
+import '../styles/overview.css';
+import '../styles/sidebar.css';
+import '../styles/transactions.css';
+import '../styles/transactions-detail.css';
+import '../styles/edit-profile.css';
+import '../styles/navbar-log-in.css';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -42,7 +43,8 @@ function MyApp({ Component, pageProps }: AppProps) {
         />
         <script src="https://unpkg.com/aos@2.3.1/dist/aos.js" />
       </Head>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
+      ;
     </>
   );
 }
